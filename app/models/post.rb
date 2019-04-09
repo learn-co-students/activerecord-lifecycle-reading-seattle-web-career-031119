e03a79d25b8e06b0756d95406c4d56aac037473c
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-04-08 21:17:24 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+  ## use before validation when you are changing a model attribute, like changing the case or format
+
+  # before_save :email_author_about_post
+  ## use before save when you are doing something that doesn't change a model attribute - like sending an email
 
   private
 
